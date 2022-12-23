e479b0e8a98b48182b449b990e5e2e6596da8865
PJ Fanning <pjfanning@users.noreply.github.com>
2022-12-23 11:21:47 +0100

    fix license

--- project/MetaInfLicenseNoticeCopy.scala
+++ project/MetaInfLicenseNoticeCopy.scala
@@ -1,5 +1,10 @@
 /*
- * Copyright (C) 2016-2022 Lightbend Inc. <https://www.lightbend.com>
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
  */
 
 import sbt.Keys._
