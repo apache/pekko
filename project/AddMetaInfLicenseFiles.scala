751ac2f2e365dc0e01da6c8a95deb6486683f37e
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-02-12 14:14:49 +0100

    Use sbt-apache-sonatype for Apache Nexus repo settings

---
+++ project/AddMetaInfLicenseFiles.scala
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+import sbt.Keys._
+import sbt._
+import org.mdedetrich.apache.sonatype.SonatypeApachePlugin
+import org.mdedetrich.apache.sonatype.SonatypeApachePlugin.autoImport._
+
+/**
+ * Copies LICENSE and NOTICE files into jar META-INF dir
+ */
+object AddMetaInfLicenseFiles extends AutoPlugin {
+
+  private lazy val baseDir = LocalRootProject / baseDirectory
+
+  override lazy val projectSettings = Seq(
+    apacheSonatypeLicenseFile := baseDir.value / "legal" / "StandardLicense.txt",
+    apacheSonatypeDisclaimerFile := Some(baseDir.value / "DISCLAIMER"))
+
+  /**
+   * Settings specific for Pekko protobuf subprojects which require a different license file
+   * as well as an additional "COPYING.protobuf" file.
+   */
+  lazy val protobufSettings = Seq(
+    apacheSonatypeLicenseFile := baseDir.value / "LICENSE") ++ inConfig(Compile)(Seq(
+    resourceGenerators += {
+      Def.task {
+        List(
+          SonatypeApachePlugin.addFileToMetaInf(resourceManaged.value, baseDir.value / "COPYING.protobuf"))
+      }
+    }))
+
+  override def trigger = allRequirements
+
+  override def requires = SonatypeApachePlugin
+}
