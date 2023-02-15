158dd928c53af90d72eb593af01565c629d73252
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-02-15 14:36:19 +0100

    remove hash.sh

--- scripts/release/build-release.sh
+++ scripts/release/build-release.sh
@@ -16,7 +16,7 @@ git checkout $BUILD_COMMIT
 sbt sourceDistGenerate
 
 # sign source artifacts
-find target/dist -regex '.*\(tgz\|zip\)' | xargs -n1 /scripts/hash.sh
+find target/dist -regex '.*\(tgz\|zip\)' | xargs -I{} sh -c "sha512sum {} > {}.sha512"
 find target/dist -regex '.*\(tgz\|zip\)' | xargs -n1 gpg --sign --armor --default-key $GPG_SIGNING_KEY --detach-sig
 
 # upload source artifacts
--- scripts/release/hash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-set -o pipefail
-
-sha512sum $1 > $1.sha512
