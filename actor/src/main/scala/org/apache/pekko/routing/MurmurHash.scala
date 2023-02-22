de1a5e3b373efb9df361ec5b8412ef3c3d5b259b
PJ Fanning <pjfanning@users.noreply.github.com>
2023-02-22 16:10:24 +0100

    acknowledge smhasher copyright but add apache license header (#159)
    * acknowledge lamp/epfl copyright  but add apache license header
    
    * scalafmt
    
    * latest attempt
    
    * Update LICENSE
    
    * add Austin Appleby attribution
    
    * remove debian check
    
    * Update CopyrightHeader.scala
    
    * add source license changes for MurmurHash and introduce more jar specific licenses
    
    * scalafmt
    
    * update comment

--- actor/src/main/scala/org/apache/pekko/routing/MurmurHash.scala
+++ actor/src/main/scala/org/apache/pekko/routing/MurmurHash.scala
@@ -1,3 +1,17 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
+//-----------------------------------------------------------------------------
+// MurmurHash3 was written by Austin Appleby, and is placed in the public
+// domain. The author hereby disclaims copyright to this source code.
+//-----------------------------------------------------------------------------
+
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
 **    / __/ __// _ | / /  / _ |    (c) 2003-2011, LAMP/EPFL             **
--- project/AddMetaInfLicenseFiles.scala
+++ project/AddMetaInfLicenseFiles.scala
@@ -24,17 +24,37 @@ object AddMetaInfLicenseFiles extends AutoPlugin {
     apacheSonatypeDisclaimerFile := Some(baseDir.value / "DISCLAIMER"))
 
   /**
-   * Settings specific for Pekko protobuf subprojects which require a different license file
+   * Settings specific for Pekko actor subproject which requires a different license file.
+   */
+  lazy val actorSettings = Seq(
+    apacheSonatypeLicenseFile := baseDir.value / "legal" / "pekko-actor-jar-license.txt")
+
+  /**
+   * Settings specific for Pekko protobuf subproject which requires a different license file
    * as well as an additional "COPYING.protobuf" file.
    */
   lazy val protobufSettings = Seq(
-    apacheSonatypeLicenseFile := baseDir.value / "LICENSE") ++ inConfig(Compile)(Seq(
+    apacheSonatypeLicenseFile := baseDir.value / "legal" / "pekko-protobuf-jar-license.txt") ++ inConfig(Compile)(Seq(
     resourceGenerators += {
       Def.task {
         List(
           SonatypeApachePlugin.addFileToMetaInf(resourceManaged.value, baseDir.value / "COPYING.protobuf"))
       }
     }))
+
+  /**
+   * Settings specific for Pekko protobuf-v3 subproject which requires a different license file
+   * as well as an additional "COPYING.protobuf" file.
+   */
+  lazy val protobufV3Settings = Seq(
+    apacheSonatypeLicenseFile := baseDir.value / "legal" / "pekko-protobuf-v3-jar-license.txt") ++ inConfig(Compile)(
+    Seq(
+      resourceGenerators += {
+        Def.task {
+          List(
+            SonatypeApachePlugin.addFileToMetaInf(resourceManaged.value, baseDir.value / "COPYING.protobuf"))
+        }
+      }))
 
   override def trigger = allRequirements
 
