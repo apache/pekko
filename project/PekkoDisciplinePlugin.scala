96d53a23750620c9fa254c8548ef6f00dcc3344b
PJ Fanning <pjfanning@users.noreply.github.com>
2023-03-27 16:44:02 +0200

    Update PekkoDisciplinePlugin.scala

--- project/PekkoDisciplinePlugin.scala
+++ project/PekkoDisciplinePlugin.scala
@@ -48,6 +48,8 @@ object PekkoDisciplinePlugin extends AutoPlugin {
     // references to deprecated PARSER fields in generated message formats?
     "pekko-persistence-query",
     "pekko-docs",
+    // references to deprecated Jackson methods that would involve a significant refactor to avoid
+    "pekko-serialization-jackson",
     // use varargs of `Graph` in alsoTo and etc operators
     "pekko-stream-tests")
 
