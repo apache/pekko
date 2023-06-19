08cf7cbdac0201e00f933c4e263e4bb7cd906c87
PJ Fanning <pjfanning@users.noreply.github.com>
2023-06-19 18:35:38 +0100

    add scala epfl license header

--- actor/src/main/scala-2.13+/org/apache/pekko/util/ccompat/package.scala
+++ actor/src/main/scala-2.13+/org/apache/pekko/util/ccompat/package.scala
@@ -11,6 +11,18 @@
  * Copyright (C) 2018-2022 Lightbend Inc. <https://www.lightbend.com>
  */
 
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 package org.apache.pekko.util
 
 /**
