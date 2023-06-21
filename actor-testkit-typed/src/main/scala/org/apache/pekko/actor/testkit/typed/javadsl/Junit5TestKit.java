9e68f057656d1dccc5c9fa04815645c546455c7e
thomas <thomas@muellerleile.info>
2023-06-21 08:17:39 +0200

    junit5 extension and tests

---
+++ actor-testkit-typed/src/main/scala/org/apache/pekko/actor/testkit/typed/javadsl/Junit5TestKit.java
@@ -0,0 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
+package org.apache.pekko.actor.testkit.typed.javadsl;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public  @interface Junit5TestKit {
+}
+
+
---
+++ actor-testkit-typed/src/test/java/org/apache/pekko/actor/testkit/typed/javadsl/ActorTestKitJunit5Test.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2018-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package org.apache.pekko.actor.testkit.typed.javadsl;
+
+import org.apache.pekko.Done;
+import org.apache.pekko.actor.typed.javadsl.Behaviors;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.scalatestplus.junit.JUnitSuite;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.pekko.Done.done;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayName("ActorTestKitTestJunit5")
+@ExtendWith(TestKitJunit5Extension.class)
+@ExtendWith(LogCapturingExtension.class)
+class ActorTestKitJunit5Test extends JUnitSuite {
+
+  @Junit5TestKit public ActorTestKit testKit = new Junit5TestKitBuilder().build();
+
+  @Test
+  void systemNameShouldComeFromTestClassViaJunitResource() {
+    assertEquals("ActorTestKitJunit5Test", testKit.system().name());
+  }
+
+  @Test
+  void systemNameShouldComeFromTestClass() {
+    final ActorTestKit testKit2 = ActorTestKit.create();
+    try {
+      assertEquals("ActorTestKitJunit5Test", testKit2.system().name());
+    } finally {
+      testKit2.shutdownTestKit();
+    }
+  }
+
+  @Test
+  void systemNameShouldComeFromGivenClassName() {
+    final ActorTestKit testKit2 = ActorTestKit.create(HashMap.class.getName());
+    try {
+      // removing package name and such
+      assertEquals("HashMap", testKit2.system().name());
+    } finally {
+      testKit2.shutdownTestKit();
+    }
+  }
+
+  @Test
+  void testKitShouldSpawnActor() throws Exception {
+    final CompletableFuture<Done> started = new CompletableFuture<>();
+    testKit.spawn(
+        Behaviors.setup(
+            (context) -> {
+              started.complete(done());
+              return Behaviors.same();
+            }));
+    assertNotNull(started.get(3, TimeUnit.SECONDS));
+  }
+}
