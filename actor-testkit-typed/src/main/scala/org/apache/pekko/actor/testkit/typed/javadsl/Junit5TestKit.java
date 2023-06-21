56111b42331d13221d7ec21fbd4c5b72c145517f
thomas <thomas@muellerleile.info>
2023-06-21 07:55:15 +0200

    Added support for Junit5 through a Junit5 extension, added tests and doc

---
+++ actor-testkit-typed/src/main/scala/org/apache/pekko/actor/testkit/typed/javadsl/Junit5TestKit.java
@@ -0,0 +1,11 @@
+package org.apache.pekko.actor.testkit.typed.javadsl;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public  @interface Junit5TestKit {
+}
+
+
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,6 +24,8 @@ object Dependencies {
     .withRank(KeyRanks.Invisible) // avoid 'unused key' warning
 
   val junitVersion = "4.13.2"
+  val junit5Version = "5.10.0-M1"
+
   val slf4jVersion = "1.7.36"
   // check agrona version when updating this
   val aeronVersion = "1.38.1"
@@ -86,6 +88,8 @@ object Dependencies {
     val lmdb = "org.lmdbjava" % "lmdbjava" % "0.7.0"
 
     val junit = "junit" % "junit" % junitVersion
+    val junit5 = "org.junit.jupiter" % "junit-jupiter-engine" % junit5Version
+
 
     // For Java 8 Conversions
     val java8Compat = Def.setting {
@@ -138,8 +142,9 @@ object Dependencies {
       val commonsIo = "commons-io" % "commons-io" % "2.11.0" % Test
       val commonsCodec = "commons-codec" % "commons-codec" % "1.15" % Test
       val commonsCompress = "org.apache.commons" % "commons-compress" % "1.23.0" % Test
-      val junit = "junit" % "junit" % junitVersion % Test
       val httpClient = "org.apache.httpcomponents" % "httpclient" % "4.5.14" % Test
+      val junit = "junit" % "junit" % junitVersion % "test"
+      val junit5 = "org.junit.jupiter" % "junit-jupiter-engine" % junit5Version % Test
 
       val logback = Compile.logback % Test
 
@@ -170,16 +175,6 @@ object Dependencies {
 
       // docker utils
       val dockerClient = "com.spotify" % "docker-client" % "8.16.0" % Test
-
-      val jackson = Def.setting {
-        Seq(
-          (jacksonCore.value % Test).force(),
-          (jacksonAnnotations.value % Test).force(),
-          (jacksonDatabind.value % Test).force(),
-          ("com.fasterxml.jackson.jaxrs" % "jackson-jaxrs-base" % jacksonCoreVersion % Test).force(),
-          ("com.fasterxml.jackson.jaxrs" % "jackson-jaxrs-json-provider" % jacksonCoreVersion % Test).force(),
-          ("com.fasterxml.jackson.datatype" % "jackson-datatype-guava" % jacksonCoreVersion % Test).force())
-      }
 
       // metrics, measurements, perf testing
       val metrics = "io.dropwizard.metrics" % "metrics-core" % "4.2.10" % Test
@@ -213,6 +208,7 @@ object Dependencies {
       val levelDBNative = "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8" % "optional;provided"
 
       val junit = Compile.junit % "optional;provided;test"
+      val junit5 = Compile.junit5 % "optional;provided;test"
 
       val scalatest = Def.setting { "org.scalatest" %% "scalatest" % scalaTestVersion % "optional;provided;test" }
 
@@ -266,6 +262,7 @@ object Dependencies {
   val actorTestkitTyped = l ++= Seq(
     Provided.logback,
     Provided.junit,
+    Provided.junit5,
     Provided.scalatest.value,
     TestDependencies.scalatestJUnit.value)
 
