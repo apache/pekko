92e8cc39f97b79752a5da42b3c468f2863fec1cb
thomas <thomas@muellerleile.info>
2023-06-21 21:49:47 +0200

    added results of scalafmt run

--- actor-testkit-typed/src/main/scala/org/apache/pekko/actor/testkit/typed/javadsl/TestKitJunit5Extension.scala
+++ actor-testkit-typed/src/main/scala/org/apache/pekko/actor/testkit/typed/javadsl/TestKitJunit5Extension.scala
@@ -10,7 +10,7 @@
 package org.apache.pekko.actor.testkit.typed.javadsl
 
 import org.apache.pekko
-import org.junit.jupiter.api.extension.{AfterAllCallback, BeforeTestExecutionCallback, ExtensionContext}
+import org.junit.jupiter.api.extension.{ AfterAllCallback, BeforeTestExecutionCallback, ExtensionContext }
 import org.junit.platform.commons.support.AnnotationSupport
 
 import scala.jdk.CollectionConverters._
@@ -19,12 +19,10 @@ final class TestKitJunit5Extension() extends AfterAllCallback with BeforeTestExe
 
   var testKit: Option[ActorTestKit] = None
 
-
   /**
    * Get a reference to the field annotated with @Junit5Testkit  [[pekko.actor.testkit.typed.javadsl.Junit5TestKit]]
-   *
    */
-  override def beforeTestExecution(context: ExtensionContext): Unit =  {
+  override def beforeTestExecution(context: ExtensionContext): Unit = {
 
     context.getTestInstance.ifPresent(instance => {
       val fielValue = AnnotationSupport.findAnnotatedFieldValues(instance, classOf[Junit5TestKit]).asScala.toList.head
@@ -32,12 +30,11 @@ final class TestKitJunit5Extension() extends AfterAllCallback with BeforeTestExe
     })
   }
 
-
   /**
    * Shutdown testKit
    */
   override def afterAll(context: ExtensionContext): Unit = {
-     testKit.get.shutdownTestKit()
+    testKit.get.shutdownTestKit()
   }
 
 }
