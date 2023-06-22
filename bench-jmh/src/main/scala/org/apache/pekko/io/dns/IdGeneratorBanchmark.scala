b0fdac259bd57fdd481483f3fe9a7aec6e1ff38a
PJ Fanning <pjfanning@users.noreply.github.com>
2023-06-22 14:19:26 +0100

    adjust ASF header (#450)

--- bench-jmh/src/main/scala/org/apache/pekko/io/dns/IdGeneratorBanchmark.scala
+++ bench-jmh/src/main/scala/org/apache/pekko/io/dns/IdGeneratorBanchmark.scala
@@ -4,7 +4,7 @@
  *
  *   https://www.apache.org/licenses/LICENSE-2.0
  *
- * This file is part of the Apache Pekko project, derived from Akka.
+ * This file is part of the Apache Pekko project, which was derived from Akka.
  */
 
 package org.apache.pekko.io.dns
--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@
  *
  *   https://www.apache.org/licenses/LICENSE-2.0
  *
- * This file is part of the Apache Pekko project, derived from Akka.
+ * This file is part of the Apache Pekko project, which was derived from Akka.
  */
 
 import org.apache.pekko._
