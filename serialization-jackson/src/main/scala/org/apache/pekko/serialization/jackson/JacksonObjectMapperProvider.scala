0509754cd60de66cce0a06137c354c98f44246b3
PJ Fanning <pjfanning@users.noreply.github.com>
2023-08-12 14:18:03 +0100

    Update JacksonObjectMapperProvider.scala
    Update JacksonObjectMapperProvider.scala
    
    Update JacksonObjectMapperProvider.scala
    
    Update JacksonObjectMapperProvider.scala

--- serialization-jackson/src/main/scala/org/apache/pekko/serialization/jackson/JacksonObjectMapperProvider.scala
+++ serialization-jackson/src/main/scala/org/apache/pekko/serialization/jackson/JacksonObjectMapperProvider.scala
@@ -17,34 +17,41 @@ import java.util.Optional
 import java.util.concurrent.ConcurrentHashMap
 import scala.annotation.nowarn
 import scala.collection.immutable
-import scala.util.Failure
-import scala.util.Success
-import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.PropertyAccessor
-import com.fasterxml.jackson.core.{JsonFactory, JsonFactoryBuilder, JsonGenerator, JsonParser, StreamReadConstraints, StreamReadFeature, StreamWriteFeature}
-import com.fasterxml.jackson.core.json.JsonReadFeature
-import com.fasterxml.jackson.core.json.JsonWriteFeature
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.MapperFeature
-import com.fasterxml.jackson.databind.Module
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
+import scala.util.{ Failure, Success }
+import com.fasterxml.jackson.annotation.{ JsonAutoDetect, JsonCreator, PropertyAccessor }
+import com.fasterxml.jackson.core.{
+  JsonFactory,
+  JsonFactoryBuilder,
+  JsonGenerator,
+  JsonParser,
+  StreamReadConstraints,
+  StreamReadFeature,
+  StreamWriteFeature
+}
+import com.fasterxml.jackson.core.json.{ JsonReadFeature, JsonWriteFeature }
+import com.fasterxml.jackson.databind.{
+  DeserializationFeature,
+  MapperFeature,
+  Module,
+  ObjectMapper,
+  SerializationFeature
+}
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import com.typesafe.config.Config
 import org.apache.pekko
-import pekko.actor.ActorSystem
-import pekko.actor.ClassicActorSystemProvider
-import pekko.actor.DynamicAccess
-import pekko.actor.ExtendedActorSystem
-import pekko.actor.Extension
-import pekko.actor.ExtensionId
-import pekko.actor.ExtensionIdProvider
+import pekko.actor.{
+  ActorSystem,
+  ClassicActorSystemProvider,
+  DynamicAccess,
+  ExtendedActorSystem,
+  Extension,
+  ExtensionId,
+  ExtensionIdProvider
+}
 import pekko.actor.setup.Setup
 import pekko.annotation.InternalStableApi
-import pekko.event.Logging
-import pekko.event.LoggingAdapter
+import pekko.event.{ Logging, LoggingAdapter }
 import pekko.util.unused
 import pekko.util.OptionConverters._
 
@@ -62,7 +69,7 @@ object JacksonObjectMapperProvider extends ExtensionId[JacksonObjectMapperProvid
    */
   def configForBinding(bindingName: String, systemConfig: Config): Config = {
     val basePath = "pekko.serialization.jackson"
-    val baseConf = systemConfig.getConfig("pekko.serialization.jackson")
+    val baseConf = systemConfig.getConfig(basePath)
     if (systemConfig.hasPath(s"$basePath.$bindingName"))
       systemConfig.getConfig(s"$basePath.$bindingName").withFallback(baseConf)
     else
