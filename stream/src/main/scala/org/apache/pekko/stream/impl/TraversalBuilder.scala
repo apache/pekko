580f12c29fb61b65758af9b0c31494af0af17175
Dongxu Wang <dongxu@apache.org>
2023-08-18 07:07:52 +0800

    clean GraphAssembly in scaladoc since which has gone for a while (#579)

--- stream/src/main/scala/org/apache/pekko/stream/impl/TraversalBuilder.scala
+++ stream/src/main/scala/org/apache/pekko/stream/impl/TraversalBuilder.scala
@@ -38,8 +38,7 @@ import pekko.util.unused
  * number (and those which are not receive different numbers). This feature can be used to
  *  - materialize a graph, using the slots as indices to an array of Publishers/Subscribers that need to be wired
  *    together
- *  - fuse a graph, using the slots to construct a [[pekko.stream.impl.fusing.GraphInterpreter.GraphAssembly]] which
- *    uses a similar layout
+ *  - fuse a graph
  *  - create a DOT formatted output for visualization
  *  - convert the graph to another data structure
  *
