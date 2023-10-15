694ebb456ae5a3cbf9e997eb4c6070cdef9cdd0a
PJ Fanning <pjfanning@users.noreply.github.com>
2023-10-15 02:21:53 +0100

    use bcpkix-jdk18on (jdk15on no longer published)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -133,7 +133,7 @@ object Dependencies {
     }
 
     object TestDependencies {
-      val bcpkix = "org.bouncycastle" % "bcpkix-jdk15on" % "1.70" % Test
+      val bcpkix = "org.bouncycastle" % "bcpkix-jdk18on" % "1.76" % Test
       val commonsMath = "org.apache.commons" % "commons-math" % "2.2" % Test
       val commonsIo = "commons-io" % "commons-io" % "2.11.0" % Test
       val commonsCodec = "commons-codec" % "commons-codec" % "1.15" % Test
