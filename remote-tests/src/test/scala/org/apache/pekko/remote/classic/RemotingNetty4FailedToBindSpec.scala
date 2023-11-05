64ac9c57c364f0ba9a038fc5055362f85ec04ba0
PJ Fanning <pjfanning@users.noreply.github.com>
2023-11-05 21:48:49 +0100

    Create RemotingNetty4FailedToBindSpec.scala

---
+++ remote-tests/src/test/scala/org/apache/pekko/remote/classic/RemotingNetty4FailedToBindSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2018-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package org.apache.pekko.remote.classic
+
+import com.typesafe.config.ConfigFactory
+import org.apache.pekko
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.testkit.SocketUtil
+import org.jboss.netty.channel.ChannelException
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class RemotingNetty4FailedToBindSpec extends AnyWordSpec with Matchers {
+
+  "an ActorSystem" must {
+    "not start if port is taken (Netty 4)" in {
+      val port = SocketUtil.temporaryLocalPort()
+      val config = ConfigFactory.parseString(s"""
+           |pekko {
+           |  actor {
+           |    provider = remote
+           |  }
+           |  remote.artery.enabled = off
+           |  remote.classic.netty {
+           |    ssl {
+           |      ssl-engine-provider = org.apache.pekko.remote.transport.netty4.ConfigSSLEngineProvider
+           |    }
+           |    tcp {
+           |      hostname = "127.0.0.1"
+           |      port = $port
+           |      transport-class = "org.apache.pekko.remote.transport.netty4.NettyTransport"
+           |    }
+           |  }
+           |}
+       """.stripMargin)
+      val as = ActorSystem("RemotingFailedToBindSpec", config)
+      try {
+        val ex = intercept[ChannelException] {
+          ActorSystem("BindTest2", config)
+        }
+        ex.getMessage should startWith("Failed to bind")
+      } finally {
+        as.terminate()
+      }
+    }
+  }
+}
