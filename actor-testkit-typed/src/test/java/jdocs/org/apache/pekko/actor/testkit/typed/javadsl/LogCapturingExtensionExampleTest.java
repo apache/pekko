153b9c251e1406175f8a7c1f57c85d6fbca9c453
Thomas Müllerleile <thomas.muellerleile@nvaria.ch>
2023-11-10 11:51:55 +0100

    [pekko-testkit-typed] Support for JUnit5 in testkit  (#751)
    * JUnit5 support for testkit typed
    
    * renamed TestKitJunitResource to TestKitJUnitResource
    
    * renamed  "JUnit" to "Junit" to adhere to previous standard
    
    * Cleanup remaining residuals of renaming from JUnit to Junit
    
    * headers and scalafmt
    
    * Change Junit5 to JUnit5
    
    * revert @unused annotation
    
    * scalafmt
    
    * javafmt
    
    * scalaify use of java optional
    
    * add newline
    
    * revert changes to Events.scala
    
    * replace Option.when with if statement
    
    ---------
    
    Co-authored-by: thomas <thomas@muellerleile.info>

---
+++ actor-testkit-typed/src/test/java/jdocs/org/apache/pekko/actor/testkit/typed/javadsl/LogCapturingExtensionExampleTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jdocs.org.apache.pekko.actor.testkit.typed.javadsl;
+
+import org.apache.pekko.actor.testkit.typed.annotations.JUnit5TestKit;
+import org.apache.pekko.actor.testkit.typed.javadsl.*;
+import org.apache.pekko.actor.testkit.typed.javadsl.JUnit5TestKitBuilder;
+import org.apache.pekko.actor.typed.ActorRef;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static jdocs.org.apache.pekko.actor.testkit.typed.javadsl.AsyncTestingExampleTest.Echo;
+
+// test code copied from LogCapturingExampleTest.java
+
+@DisplayName("JUnit5 log capturing")
+@ExtendWith(TestKitJUnit5Extension.class)
+@ExtendWith(LogCapturingExtension.class)
+class LogCapturingExtensionExampleTest {
+
+  @JUnit5TestKit public ActorTestKit testKit = new JUnit5TestKitBuilder().build();
+
+  @Test
+  void testSomething() {
+    ActorRef<Echo.Ping> pinger = testKit.spawn(Echo.create(), "ping");
+    TestProbe<Echo.Pong> probe = testKit.createTestProbe();
+    pinger.tell(new Echo.Ping("hello", probe.ref()));
+    probe.expectMessage(new Echo.Pong("hello"));
+  }
+
+  @Test
+  void testSomething2() {
+    ActorRef<Echo.Ping> pinger = testKit.spawn(Echo.create(), "ping");
+    TestProbe<Echo.Pong> probe = testKit.createTestProbe();
+    pinger.tell(new Echo.Ping("hello", probe.ref()));
+    probe.expectMessage(new Echo.Pong("hello"));
+  }
+}
+// #log-capturing-junit5
---
+++ actor-testkit-typed/src/test/scala/org/apache/pekko/actor/testkit/typed/scaladsl/GreeterMain.scala
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+package org.apache.pekko.actor.testkit.typed.scaladsl
+
+import org.apache.pekko
+import pekko.actor.typed.{ ActorRef, Behavior }
+import pekko.actor.typed.scaladsl.Behaviors
+
+object Greeter {
+  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+  final case class Greeted(whom: String, from: ActorRef[Greet])
+
+  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
+    context.log.info("Hello {}!", message.whom)
+    // #greeter-send-messages
+    message.replyTo ! Greeted(message.whom, context.self)
+    // #greeter-send-messages
+    Behaviors.same
+  }
+}
+
+object GreeterBot {
+
+  def apply(max: Int): Behavior[Greeter.Greeted] = {
+    bot(0, max)
+  }
+
+  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
+    Behaviors.receive { (context, message) =>
+      val n = greetingCounter + 1
+      context.log.info("Greeting {} for {}", n, message.whom)
+      if (n == max) {
+        Behaviors.stopped
+      } else {
+        message.from ! Greeter.Greet(message.whom, context.self)
+        bot(n, max)
+      }
+    }
+}
+
+object GreeterMain {
+
+  final case class SayHello(name: String)
+
+  def apply(): Behavior[SayHello] =
+    Behaviors.setup { context =>
+      val greeter = context.spawn(Greeter(), "greeter")
+
+      Behaviors.receiveMessage { message =>
+        val replyTo = context.spawn(GreeterBot(max = 3), message.name)
+        greeter ! Greeter.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+    }
+}
