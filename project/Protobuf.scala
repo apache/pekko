805b9cbc89fcd2d5b3f615445c1ba0c479ce69e2
Martin Duhem <mduhem@gradle.com>
2024-02-02 16:20:41 +0100

    Cache output of assembly
    Previously, the build would rebuild over and over the uber jar with the
    shaded protobuf3 distribution. With this patch, this is no longer the
    case, as the output is cached locally.

--- project/Protobuf.scala
+++ project/Protobuf.scala
@@ -38,7 +38,7 @@ object Protobuf {
     outputPaths := Seq((Compile / sourceDirectory).value, (Test / sourceDirectory).value).map(_ / "java"),
     importPath := None,
     // this keeps intellij happy for files that use the shaded protobuf
-    Compile / unmanagedJars += (LocalProject("protobuf-v3") / assembly).value,
+    Compile / unmanagedJars += (LocalProject("protobuf-v3") / Compile / packageBin).value,
     protoc := "protoc",
     protocVersion := "3.11.4",
     generate := {
