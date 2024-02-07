76e1b0095f2b9dbdf9e5a00613f2b31b0e65f102
PJ Fanning <pjfanning@users.noreply.github.com>
2024-02-07 12:44:26 +0100

    protobuf-java 3.23.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
   val agronaVersion = "1.19.2"
   val nettyVersion = "4.1.106.Final"
-  val protobufJavaVersion = "3.22.5"
+  val protobufJavaVersion = "3.23.4"
   val logbackVersion = "1.3.14"
 
   val jacksonCoreVersion = "2.16.1"
