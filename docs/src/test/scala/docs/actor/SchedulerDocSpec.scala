e868ce6392bb6285ea4a243ffa5f34dffd88d0d4
He-Pin(kerr) <hepin1989@gmail.com>
2024-03-24 00:27:25 +0800

    chore: Fix scala constructor finding tests for scala 3. (#1220)
    * chore: Fix scala constructor finding tests for scala 3.
    
    * reorder imports
    
    (cherry picked from commit c818001180c52fe89037bfc76421ef8ad9cc81aa)

--- docs/src/test/scala/docs/actor/SchedulerDocSpec.scala
+++ docs/src/test/scala/docs/actor/SchedulerDocSpec.scala
@@ -13,6 +13,8 @@
 
 package docs.actor
 
+import docs.actor.SchedulerDocSpec.TickActor
+
 import language.postfixOps
 
 //#imports1
@@ -24,6 +26,16 @@ import scala.concurrent.duration._
 //#imports1
 
 import pekko.testkit._
+
+object SchedulerDocSpec {
+  // #schedule-recurring
+  class TickActor extends Actor {
+    def receive: Receive = {
+      case "tick" => // Do something
+    }
+  }
+  // #schedule-recurring
+}
 
 class SchedulerDocSpec extends PekkoSpec(Map("pekko.loglevel" -> "INFO")) {
   "schedule a one-off task" in {
@@ -47,27 +59,20 @@ class SchedulerDocSpec extends PekkoSpec(Map("pekko.loglevel" -> "INFO")) {
   }
 
   "schedule a recurring task" in {
-    new AnyRef {
-      // #schedule-recurring
-      val Tick = "tick"
-      class TickActor extends Actor {
-        def receive = {
-          case Tick => // Do something
-        }
-      }
-      val tickActor = system.actorOf(Props(classOf[TickActor], this))
-      // Use system's dispatcher as ExecutionContext
-      import system.dispatcher
+    // #schedule-recurring
 
-      // This will schedule to send the Tick-message
-      // to the tickActor after 0ms repeating every 50ms
-      val cancellable =
-        system.scheduler.scheduleWithFixedDelay(Duration.Zero, 50.milliseconds, tickActor, Tick)
+    val tickActor = system.actorOf(Props(classOf[TickActor]))
+    // Use system's dispatcher as ExecutionContext
+    import system.dispatcher
 
-      // This cancels further Ticks to be sent
-      cancellable.cancel()
-      // #schedule-recurring
-      system.stop(tickActor)
-    }
+    // This will schedule to send the Tick-message
+    // to the tickActor after 0ms repeating every 50ms
+    val cancellable =
+      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 50.milliseconds, tickActor, "tick")
+
+    // This cancels further Ticks to be sent
+    cancellable.cancel()
+    // #schedule-recurring
+    system.stop(tickActor)
   }
 }
