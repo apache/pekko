27e3b3cbb3bc94a611f795beee7abc3448735745
PJ Fanning <pjfanning@users.noreply.github.com>
2024-12-04 20:28:20 +0100

    Update ArterySettings.scala

--- remote/src/main/scala/org/apache/pekko/remote/artery/ArterySettings.scala
+++ remote/src/main/scala/org/apache/pekko/remote/artery/ArterySettings.scala
@@ -106,9 +106,9 @@ private[pekko] final class ArterySettings private (config: Config) {
   val Version: Byte = ArteryTransport.HighestVersion
 
   /**
-    * If set to true, harmless quarantine events are propagated in InboundQuarantineCheck.
-    * Background is in https://github.com/apache/pekko/pull/1555
-    */
+   * If set to true, harmless quarantine events are propagated in InboundQuarantineCheck.
+   * Background is in https://github.com/apache/pekko/pull/1555
+   */
   val PropagateHarmlessQuarantineEvents: Boolean =
     getBoolean("propagate-harmless-quarantine-events")
 
