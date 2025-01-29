425b3f90a04a7f98d57e8eb638b457ca081f0110
PJ Fanning <pjfanning@users.noreply.github.com>
2025-01-29 20:24:45 +0100

    [CI] fix issue with runninf scala-cli scripts (#1752)
    try to pin scala in scala-cli
    
    more hacking
    
    Update VerifyJDK9Classes.scala
    
    Update verify-jdk9-classes.sh
    
    update
    
    revert some changes
    
    Update nightly-builds.yml
    
    Update nightly-builds.yml

--- .github/workflows/verify-jdk9-classes.sh
+++ .github/workflows/verify-jdk9-classes.sh
@@ -17,12 +17,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
-echo "Checking Scala CLI version..."
-scala-cli --version
-
 echo "Starting verification with Scala-CLI..."
 
 for file in stream/target/scala-cli/*.sc; do
+  cat $file
   echo "Starting verification for with file: $file."
   if scala-cli "$file" ; then
     echo "Verification successful for $file."
