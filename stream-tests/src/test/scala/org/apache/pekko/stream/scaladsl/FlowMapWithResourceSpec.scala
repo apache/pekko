a15082f46e9f0a818a5d930fa929b53597be813d
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-07-14 22:22:24 +0200

    fix import formatting

--- stream-tests/src/test/scala/org/apache/pekko/stream/scaladsl/FlowMapWithResourceSpec.scala
+++ stream-tests/src/test/scala/org/apache/pekko/stream/scaladsl/FlowMapWithResourceSpec.scala
@@ -21,23 +21,26 @@ import java.io.BufferedReader
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.util.concurrent.atomic.AtomicInteger
-import scala.annotation.{nowarn, tailrec}
+
+import scala.annotation.{ nowarn, tailrec }
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.{Await, Promise}
+import scala.concurrent.{ Await, Promise }
 import scala.concurrent.duration.DurationInt
 import scala.util.Success
 import scala.util.control.NoStackTrace
-import com.google.common.jimfs.{Configuration, Jimfs}
+
+import com.google.common.jimfs.{ Configuration, Jimfs }
+
 import org.apache.pekko
 import pekko.Done
-import pekko.stream.{AbruptTerminationException, ActorAttributes, ActorMaterializer, Attributes, SystemMaterializer}
+import pekko.stream.{ AbruptTerminationException, ActorAttributes, ActorMaterializer, Attributes, SystemMaterializer }
 import pekko.stream.ActorAttributes.supervisionStrategy
-import pekko.stream.Supervision.{restartingDecider, resumingDecider, stoppingDecider}
-import pekko.stream.impl.{PhasedFusingActorMaterializer, StreamSupervisor}
+import pekko.stream.Supervision.{ restartingDecider, resumingDecider, stoppingDecider }
+import pekko.stream.impl.{ PhasedFusingActorMaterializer, StreamSupervisor }
 import pekko.stream.impl.StreamSupervisor.Children
-import pekko.stream.testkit.{StreamSpec, TestSubscriber}
-import pekko.stream.testkit.Utils.{TE, UnboundedMailboxConfig, assertDispatcher}
-import pekko.stream.testkit.scaladsl.{TestSink, TestSource}
+import pekko.stream.testkit.{ StreamSpec, TestSubscriber }
+import pekko.stream.testkit.Utils.{ assertDispatcher, TE, UnboundedMailboxConfig }
+import pekko.stream.testkit.scaladsl.{ TestSink, TestSource }
 import pekko.testkit.EventFilter
 import pekko.util.ByteString
 
