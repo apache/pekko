8a7816f500eff54333679ce1bf566cf449ac7cd6
PJ Fanning <pjfanning@users.noreply.github.com>
2025-08-25 12:42:50 +0100

    merge issue

--- docs/src/test/java/jdocs/ddata/protobuf/TwoPhaseSetSerializer2.java
+++ docs/src/test/java/jdocs/ddata/protobuf/TwoPhaseSetSerializer2.java
@@ -49,6 +49,7 @@ public class TwoPhaseSetSerializer2 extends AbstractSerializationSupport {
 
   @Override
   public byte[] toBinary(Object obj) {
+    if (obj instanceof TwoPhaseSet tps) {
       return twoPhaseSetToProto(tps).toByteArray();
     } else {
       throw new IllegalArgumentException("Can't serialize object of type " + obj.getClass());
