d7f2e3af301beb9747a35fa59d7cdd72bc5e0fb3
PJ Fanning <pjfanning@users.noreply.github.com>
2025-08-25 17:21:42 +0100

    Update SnapshotStore.scala

--- persistence/src/main/scala/org/apache/pekko/persistence/snapshot/japi/SnapshotStore.scala
+++ persistence/src/main/scala/org/apache/pekko/persistence/snapshot/japi/SnapshotStore.scala
@@ -16,10 +16,11 @@ package org.apache.pekko.persistence.snapshot.japi
 import scala.concurrent.Future
 
 import org.apache.pekko
-import pekko.util.FutureConverters._
 import pekko.japi.Util._
 import pekko.persistence._
 import pekko.persistence.snapshot.{ SnapshotStore => SSnapshotStore }
+import pekko.util.ConstantFun
+import pekko.util.FutureConverters._
 
 /**
  * Java API: abstract snapshot store.
@@ -33,12 +34,12 @@ abstract class SnapshotStore extends SSnapshotStore with SnapshotStorePlugin {
     doLoadAsync(persistenceId, criteria).asScala.map(option)
 
   override final def saveAsync(metadata: SnapshotMetadata, snapshot: Any): Future[Unit] =
-    doSaveAsync(metadata, snapshot).asScala.map(_ => ())
+    doSaveAsync(metadata, snapshot).asScala.map(ConstantFun.unit)
 
   override final def deleteAsync(metadata: SnapshotMetadata): Future[Unit] =
-    doDeleteAsync(metadata).asScala.map(_ => ())
+    doDeleteAsync(metadata).asScala.map(ConstantFun.unit)
 
   override final def deleteAsync(persistenceId: String, criteria: SnapshotSelectionCriteria): Future[Unit] =
-    doDeleteAsync(persistenceId: String, criteria: SnapshotSelectionCriteria).asScala.map(_ => ())
+    doDeleteAsync(persistenceId: String, criteria: SnapshotSelectionCriteria).asScala.map(ConstantFun.unit)
 
 }
