db890051895bbe044707d5a4b3d45a682be3bf4e
He-Pin <hepin1989@gmail.com>
2025-08-27 01:01:17 +0800

    feat: Add Untyped actor with stash for Java 21

---
+++ actor-tests/src/test/java-jdk21-only/org/apache/pekko/actor/UntypedStashJavaAPI.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2018-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package org.apache.pekko.actor;
+
+import org.apache.pekko.testkit.PekkoJUnitActorSystemResource;
+import org.apache.pekko.testkit.TestProbe;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.scalatestplus.junit.JUnitSuite;
+
+public class UntypedStashJavaAPI extends JUnitSuite {
+
+  @ClassRule
+  public static PekkoJUnitActorSystemResource actorSystemResource =
+      new PekkoJUnitActorSystemResource("UntypedStashJavaAPI", ActorWithBoundedStashSpec.testConf());
+
+  private final ActorSystem system = actorSystemResource.getSystem();
+
+  private void testAStashApi(Props props) {
+    ActorRef ref = system.actorOf(props);
+    final TestProbe probe = new TestProbe(system);
+    probe.send(ref, "Hello");
+    probe.send(ref, "Hello2");
+    probe.send(ref, "Hello12");
+    probe.expectMsg(5);
+  }
+
+  @Test
+  public void mustBeAbleToUseStash() {
+    testAStashApi(Props.create(StashJavaAPITestUntypedActors.WithStash.class));
+  }
+
+  @Test
+  public void mustBeAbleToUseUnboundedStash() {
+    testAStashApi(Props.create(StashJavaAPITestUntypedActors.WithUnboundedStash.class));
+  }
+
+  @Test
+  public void mustBeAbleToUseUnrestrictedStash() {
+    testAStashApi(
+        Props.create(StashJavaAPITestUntypedActors.WithUnrestrictedStash.class)
+            .withMailbox("pekko.actor.mailbox.unbounded-deque-based"));
+  }
+}
