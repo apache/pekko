18aca537ded783480c4441dcc24f894a0cf6867a
PJ Fanning <pjfanning@users.noreply.github.com>
2025-09-02 13:47:04 +0100

    Annotate PekkoLoggerBridge as InternalApi (#2128)
    * Annotate PekkoLoggerBridge as InternalApi
    
    Added internal API annotations and documentation for PekkoLoggerBridge.
    
    * Deprecate ssl-config support in PekkoLoggerBridge

--- stream/src/main/scala/com/typesafe/sslconfig/pekko/util/PekkoLoggerBridge.scala
+++ stream/src/main/scala/com/typesafe/sslconfig/pekko/util/PekkoLoggerBridge.scala
@@ -15,11 +15,18 @@ package com.typesafe.sslconfig.pekko.util
 
 import org.apache.pekko
 import pekko.actor.ActorSystem
+import pekko.annotation.InternalApi
 import pekko.event.{ DummyClassForStringSources, EventStream }
 import pekko.event.Logging._
 
 import com.typesafe.sslconfig.util.{ LoggerFactory, NoDepsLogger }
 
+/**
+ * Internal Usage only. Will be removed in Pekko 2.0.0.
+ */
+@deprecated("ssl-config support will be removed in Pekko 2.0.0. Use Tcp and TLS with SSLEngine parameters instead.",
+  "1.2.1")
+@InternalApi
 final class PekkoLoggerFactory(system: ActorSystem) extends LoggerFactory {
   override def apply(clazz: Class[_]): NoDepsLogger = new PekkoLoggerBridge(system.eventStream, clazz)
 
@@ -27,6 +34,12 @@ final class PekkoLoggerFactory(system: ActorSystem) extends LoggerFactory {
     new PekkoLoggerBridge(system.eventStream, name, classOf[DummyClassForStringSources])
 }
 
+/**
+ * Internal Usage only. Will be removed in Pekko 2.0.0.
+ */
+@deprecated("ssl-config support will be removed in Pekko 2.0.0. Use Tcp and TLS with SSLEngine parameters instead.",
+  "1.2.1")
+@InternalApi
 class PekkoLoggerBridge(bus: EventStream, logSource: String, logClass: Class[_]) extends NoDepsLogger {
   def this(bus: EventStream, clazz: Class[_]) = this(bus, clazz.getCanonicalName, clazz)
 
