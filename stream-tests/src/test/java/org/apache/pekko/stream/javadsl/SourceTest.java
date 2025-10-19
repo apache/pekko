1b5175620b2352930112992bfc9d64b1e152cc66
He-Pin(kerr) <hepin1989@gmail.com>
2025-10-19 20:24:49 +0800

    feat: Add Flow#onErrorContinue operator. (#2322)
    * feat: Add Flow#onErrorContinue operator.
    
    (cherry picked from commit 87b7ae810d8caf55ff7082385f8fa54b90c62611)

--- stream-tests/src/test/java/org/apache/pekko/stream/javadsl/SourceTest.java
+++ stream-tests/src/test/java/org/apache/pekko/stream/javadsl/SourceTest.java
@@ -1666,6 +1666,24 @@ public class SourceTest extends StreamTest {
   }
 
   @Test
+  public void mustBeAbleToOnErrorContinue() {
+    Source.from(Arrays.asList(1, 2))
+        .map(
+            elem -> {
+              if (elem == 2) {
+                throw new RuntimeException("ex");
+              } else {
+                return elem;
+              }
+            })
+        .onErrorContinue(e -> logger().error(e, "Error encountered"))
+        .runWith(TestSink.probe(system), system)
+        .request(2)
+        .expectNext(1)
+        .expectComplete();
+  }
+
+  @Test
   public void mustBeAbleToOnErrorResume() {
     Source.from(Arrays.asList(1, 2))
         .map(
@@ -1696,6 +1714,25 @@ public class SourceTest extends StreamTest {
               }
             })
         .onErrorComplete(IllegalArgumentException.class)
+        .runWith(TestSink.probe(system), system)
+        .request(2)
+        .expectNext(1)
+        .expectComplete();
+  }
+
+  @Test
+  public void mustBeAbleToOnErrorContinueWithDedicatedException() {
+    Source.from(Arrays.asList(1, 2))
+        .map(
+            elem -> {
+              if (elem == 2) {
+                throw new IllegalArgumentException("ex");
+              } else {
+                return elem;
+              }
+            })
+        .onErrorContinue(
+            IllegalArgumentException.class, e -> logger().error(e, "Error encountered"))
         .runWith(TestSink.probe(system), system)
         .request(2)
         .expectNext(1)
@@ -1741,6 +1778,25 @@ public class SourceTest extends StreamTest {
   }
 
   @Test
+  public void mustBeAbleToFailWhenOnErrorContinueExceptionTypeNotMatch() {
+    final IllegalArgumentException ex = new IllegalArgumentException("ex");
+    Source.from(Arrays.asList(1, 2))
+        .map(
+            elem -> {
+              if (elem == 2) {
+                throw ex;
+              } else {
+                return elem;
+              }
+            })
+        .onErrorContinue(TimeoutException.class, e -> logger().error(e, "Error encountered"))
+        .runWith(TestSink.probe(system), system)
+        .request(2)
+        .expectNext(1)
+        .expectError(ex);
+  }
+
+  @Test
   public void onErrorResumeMustBeAbleToFailWhenExceptionTypeNotMatch() {
     final IllegalArgumentException ex = new IllegalArgumentException("ex");
     Source.from(Arrays.asList(1, 2))
@@ -1771,6 +1827,25 @@ public class SourceTest extends StreamTest {
               }
             })
         .onErrorComplete(ex -> ex.getMessage().contains("Boom"))
+        .runWith(TestSink.probe(system), system)
+        .request(2)
+        .expectNext(1)
+        .expectComplete();
+  }
+
+  @Test
+  public void mustBeAbleToOnErrorContinueWithPredicate() {
+    Source.from(Arrays.asList(1, 2))
+        .map(
+            elem -> {
+              if (elem == 2) {
+                throw new IllegalArgumentException("Boom");
+              } else {
+                return elem;
+              }
+            })
+        .onErrorContinue(
+            ex -> ex.getMessage().contains("Boom"), e -> logger().error(e, "Error encountered"))
         .runWith(TestSink.probe(system), system)
         .request(2)
         .expectNext(1)
