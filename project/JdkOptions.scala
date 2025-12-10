99f0298549f8953ad5705a51e09516a83cd6154f
PJ Fanning <pjfanning@users.noreply.github.com>
2025-12-10 12:01:00 +0100

    add job to publish 2.0 docs and add note about agrona

--- project/JdkOptions.scala
+++ project/JdkOptions.scala
@@ -26,8 +26,9 @@ object JdkOptions extends AutoPlugin {
   val targetJavaVersion = "17"
 
   lazy val versionSpecificJavaOptions =
-    // for virtual threads
+    // for Agrona UnsafeApi and virtual threads
     "--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED" ::
+    // for virtual threads
     "--add-opens=java.base/java.lang=ALL-UNNAMED" ::
     // for aeron
     "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED" ::
