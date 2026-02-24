6d8423d63097164c638d82f75702a03c44532637
Scala Steward <scala_steward@virtuslab.com>
2026-02-24 15:25:33 +0000

    Update jackson-core to 2.21.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
   val logbackVersion = "1.5.32"
 
   val jacksonAnnotationsVersion = "2.21"
-  val jacksonVersion2 = "2.21.0"
+  val jacksonVersion2 = "2.21.1"
   val jacksonVersion3 = "3.0.4"
 
   val scala213Version = "2.13.18"
