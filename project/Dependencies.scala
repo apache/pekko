0faf0ce286ab417cec7c2093a087d89f575ef733
Scala Steward <scala_steward@virtuslab.com>
2026-04-11 14:14:49 +0000

    Update lz4-java to 1.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -105,7 +105,7 @@ object Dependencies {
       .excludeAll(ExclusionRule(organization = "org.scala-lang"))
     val jacksonCbor3 = "tools.jackson.dataformat" % "jackson-dataformat-cbor" % jacksonVersion3
 
-    val lz4Java = "at.yawk.lz4" % "lz4-java" % "1.10.4"
+    val lz4Java = "at.yawk.lz4" % "lz4-java" % "1.11.0"
 
     val logback = "ch.qos.logback" % "logback-classic" % logbackVersion
 
