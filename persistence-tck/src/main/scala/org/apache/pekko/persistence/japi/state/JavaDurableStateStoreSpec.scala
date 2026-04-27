729c31531a11bc6b876f780274d749934db31df6
PJ Fanning <pjfanning@users.noreply.github.com>
2026-04-27 10:02:19 +0200

    license dates

--- persistence-tck/src/main/scala/org/apache/pekko/persistence/japi/state/JavaDurableStateStoreSpec.scala
+++ persistence-tck/src/main/scala/org/apache/pekko/persistence/japi/state/JavaDurableStateStoreSpec.scala
@@ -8,7 +8,7 @@
  */
 
 /*
- * Copyright (C) 2014-2022 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2009-2022 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package org.apache.pekko.persistence.japi.state
