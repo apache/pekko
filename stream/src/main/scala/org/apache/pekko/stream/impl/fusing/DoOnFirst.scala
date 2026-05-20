f9f03708b540cd9914018780447e2818434cc6c2
He-Pin <hepin1989@gmail.com>
2026-05-20 18:31:14 +0800

    Fuse InHandler/OutHandler into GraphStageLogic to reduce allocations
    Optimize several stream stages by fusing separate InHandler/OutHandler
    objects into the GraphStageLogic itself, reducing per-materialization
    memory allocations:
    
    1. DoOnFirst: Replace separate InHandler with boolean flag pattern
    2. Intersperse: Merge two InHandler objects (start/rest) into single fused handler
    3. Reduce: Eliminate initial InHandler by using hasFirst flag
    4. DropWithin: Replace post-timeout InHandler with timedOut flag
    5. Scan: Remove initial InHandler/OutHandler pair using initialized flag
    6. ScanAsync: Remove ZeroHandler and post-completion OutHandler
    
    Tests:
    - stream-tests/testOnly FlowDoOnFirstSpec: 2/2 passed
    - stream-tests/testOnly FlowIntersperseSpec: 8/8 passed
    - stream-tests/testOnly FlowReduceSpec: 12/12 passed
    - stream-tests/testOnly FlowDropWithinSpec: 2/2 passed
    - stream-tests/testOnly FlowScanSpec: 8/8 passed
    - stream-tests/testOnly FlowScanAsyncSpec: 19/19 passed

--- stream/src/main/scala/org/apache/pekko/stream/impl/fusing/DoOnFirst.scala
+++ stream/src/main/scala/org/apache/pekko/stream/impl/fusing/DoOnFirst.scala
@@ -36,18 +36,21 @@ import pekko.stream.stage.{ GraphStage, GraphStageLogic, InHandler, OutHandler }
 
   override def createLogic(inheritedAttributes: org.apache.pekko.stream.Attributes) =
     new GraphStageLogic(shape) with InHandler with OutHandler {
-      self =>
-      final override def onPush(): Unit = push(out, grab(in))
+      private var isFirst = true
+
+      final override def onPush(): Unit = {
+        if (isFirst) {
+          isFirst = false
+          val elem = grab(in)
+          f(elem)
+          push(out, elem)
+        } else {
+          push(out, grab(in))
+        }
+      }
+
       final override def onPull(): Unit = pull(in)
-      setHandler(out, this)
-      setHandler(in,
-        new InHandler {
-          override def onPush(): Unit = {
-            setHandler(in, self)
-            val elem = grab(in)
-            f(elem)
-            push(out, elem)
-          }
-        })
+
+      setHandlers(in, out, this)
     }
 }
