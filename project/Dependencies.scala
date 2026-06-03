7ddbc53e3a0d12007683a9a5c6d91e88c49d97ef
PJ Fanning <pjfanning@users.noreply.github.com>
2026-06-03 17:40:15 +0100

    regenerate protobuf classes with protoc 4.35.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     // The protoc version can be inferred from the Protobuf Java minor version number.
     // Example: Protobuf Java version 3.25.x uses protoc version 25.x.
     val protobufJavaVersion = "4.35.0"
-    val protocVersion = "34.1"
+    val protocVersion = "35.0"
   }
 
   val junitVersion = "4.13.2"
