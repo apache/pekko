7b10ae67dd764018ff4f49455e71a9cf65a6b08c
He-Pin(kerr) <hepin1989@gmail.com>
2026-06-04 13:28:41 +0800

    Update Scala 3 version from RC1 to RC2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   val jacksonVersion3 = "3.1.3"
 
   val scala213Version = "2.13.18"
-  val scala3Version = "3.3.8-RC1"
+  val scala3Version = "3.3.8-RC2"
   val allScalaVersions = Seq(scala213Version, scala3Version)
 
   val reactiveStreamsVersion = "1.0.4"
