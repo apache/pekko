ffc9167f62c77ff4349703b44be14709c207001e
PJ Fanning <pjfanning@users.noreply.github.com>
2026-06-23 19:50:53 +0100

    missing import

--- cluster-sharding/src/test/scala/org/apache/pekko/cluster/sharding/passivation/simulator/SimulatorSettings.scala
+++ cluster-sharding/src/test/scala/org/apache/pekko/cluster/sharding/passivation/simulator/SimulatorSettings.scala
@@ -18,6 +18,7 @@ import scala.jdk.CollectionConverters._
 
 import org.apache.pekko
 import pekko.japi.Util.immutableSeq
+import pekko.util.Helpers.toRootLowerCase
 
 import com.typesafe.config.Config
 
