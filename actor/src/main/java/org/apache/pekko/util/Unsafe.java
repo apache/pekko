22d1691d285578055adf847178d76daac33418d7
PJ Fanning <pjfanning@users.noreply.github.com>
2026-08-08 00:00:38 +0100

    compile warnings

--- actor/src/main/java/org/apache/pekko/util/Unsafe.java
+++ actor/src/main/java/org/apache/pekko/util/Unsafe.java
@@ -20,6 +20,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 /** INTERNAL API */
+@SuppressWarnings("deprecation")
 @InternalApi
 public final class Unsafe {
   public static final sun.misc.Unsafe instance;
