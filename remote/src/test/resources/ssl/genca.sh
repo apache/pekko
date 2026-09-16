70758dd3ce5aed8287bb0d0501bcfbc05d48e1ca
PJ Fanning <pjfanning@users.noreply.github.com>
2026-09-16 19:54:03 +0100

    chore: add Apache license headers to shell scripts
    Motivation:
    Eight shell scripts had no license header. Git history shows seven were
    inherited from Akka before the 2022 fork; gen-ca-bundles.sh was added new in
    Pekko (#3528).
    
    Modification:
    Add the "derived from Akka" Apache header to the seven Akka-origin scripts
    and the standard ASF header to gen-ca-bundles.sh, placed after the shebang
    where one exists. Header text copied from CopyrightHeader.scala and
    .github/scripts/resolve-scala-version.sh respectively.
    
    Result:
    All shell scripts in the repository carry an Apache license header.
    
    Tests:
    - Not run - license header only change. bash -n passes on every script.
    
    References:
    None - license header hygiene

--- remote/src/test/resources/ssl/genca.sh
+++ remote/src/test/resources/ssl/genca.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# license agreements; and to You under the Apache License, version 2.0:
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is part of the Apache Pekko project, which was derived from Akka.
+
 export PW=`cat password`
 
 # Create a self signed key pair root CA certificate.
