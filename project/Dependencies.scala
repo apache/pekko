8ef3bf7c6d2048783b9d5706957008910b4ca07d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-22 20:59:46 +0200

    Update jackson-core to 2.22.3 (#3555)
    Co-authored-by: PJ Fanning <pjfanning@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
   val logbackVersion = "1.6.3"
 
   val jacksonAnnotationsVersion = "2.22"
-  val jacksonVersion2 = "2.22.2"
+  val jacksonVersion2 = "2.22.3"
   val jacksonVersion3 = "3.2.3"
 
   val scala213Version = "2.13.18"
